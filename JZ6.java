786099e59653cb959937f6858854402b1cd61fc4
mytangyh <mytangyh@163.com>
2022-07-18 21:05:26 +0800

    Init

---
+++ JZ6.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class JZ6 {
+    class ListNode {
+       int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        
+        return null;
+    }
+}
