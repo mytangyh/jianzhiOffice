b56d99985bf1bfb13f19dab7f28a3196b7d3dc60
mytangyh <mytangyh@163.com>
2022-07-19 17:51:52 +0800

    链表基础结构

--- linkedList.java
+++ linkedList.java
@@ -1,12 +1,14 @@
 
 public class linkedList {
     public Node head;
-    public void addFirst(int data) {//头插法
+
+    public void addFirst(int data) {// 头插法
         Node newNode = new Node(data);
         newNode.next = head;
         head = newNode;
     }
-    public void addLast(int data) {//尾插法
+
+    public void addLast(int data) {// 尾插法
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
@@ -18,6 +20,7 @@ public class linkedList {
             temp.next = newNode;
         }
     }
+
     public void createListLow() {
         Node node1 = new Node(1);
         Node node2 = new Node(2);
@@ -29,14 +32,16 @@ public class linkedList {
         this.head = node1;
 
     }
-    public void show() {//遍历链表
+
+    public void show() {// 遍历链表
         Node temp = this.head;
         while (temp != null) {
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
-        
+
     }
+
     /**
      * @return
      */
@@ -48,8 +53,9 @@ public class linkedList {
             temp = temp.next;
         }
         return count;
-        
+
     }
+
     public boolean isContains(int key) {
         Node temp = this.head;
         while (temp != null) {
@@ -59,23 +65,82 @@ public class linkedList {
             temp = temp.next;
         }
         return false;
-        
+
     }
+
+    public Node searchPre(int index) {
+        Node temp = head;
+        int count = 0;
+        while (count != index - 1) {
+            temp = temp.next;
+            count++;
+        }
+        return temp;
+
+    }
+
+    public void addIndex(int index, int data) {
+        if (index < 0 || index > size()) {
+            System.out.println("不合法");
+            return;
+        }
+        if (index == 0) {
+            addFirst(data);
+        }
+        if (index == size()) {
+            addLast(data);
+        }
+        Node temp = searchPre(index);
+        Node node = new Node(data);
+        node.next = temp.next;
+        temp.next = node;
+
+    }
+
+    private void delete(int index) {
+        if (index < 0 || index > size()) {
+            System.out.println("不合法");
+            return;
+        }
+        if (index == 0) {
+            head = head.next;
+        }
+        Node temp = searchPre(index);
+        temp.next = temp.next.next;
+    }
+
+    public void clear() {
+        head = null;
+    }
+
     public static void main(String[] args) {
         linkedList list = new linkedList();
-        
-        list.show();
-        System.out.println("\n" + list.size());
+
+        // list.show();
+        // System.out.println("\n" + list.size());
         list.createListLow();
         list.show();
         System.out.println("\n" + list.size());
+        System.out.println(list.isContains(3));
+        list.addIndex(2, 5);
+        list.show();
+        System.out.println("\n" + list.size());
+        list.delete(2);
+        list.show();
+        System.out.println("\n" + list.size());
+        list.clear();
+        list.show();
+        System.out.println("\n" + list.size());
+
     }
-    
+
 }
-class Node{
+
+class Node {
     public int data;
     public Node next;
-    public Node(int data){
-        this.data=data;
+
+    public Node(int data) {
+        this.data = data;
     }
 }
